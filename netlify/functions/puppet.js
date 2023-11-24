911746c57e2cfe5eae1f10b19627942bbc3294d7
Ajay Rathod <rathodajay1202@gmail.com>
2023-11-24 16:09:17 +0530

    Added Timeout anc Changed UI

--- netlify/functions/puppet.js
+++ netlify/functions/puppet.js
@@ -6,39 +6,64 @@ chromium.setHeadlessMode = true
 chromium.setGraphicsMode = false
 
 export async function handler(event, context) {
-  try {
-    const browser = await puppeteer.launch({
-      args: chromium.args,
-      defaultViewport: chromium.defaultViewport,
-      executablePath: process.env.CHROME_EXECUTABLE_PATH ? (await chromium.executablePath('/var/task/node_modules/@sparticuz/chromium/bin')) : "C:/Program Files/Google/Chrome/Application/chrome.exe",
-    })
-
-    const page = await browser.newPage()
-    const url = JSON.parse(event.body)['url']
-    
-
-    await page.goto(url, {
-      waitUntil: "networkidle0",
-    });
-
-    // await page.setViewport({
-    //   width: 1000,
-    //   height: 600,
-    // });
-  
-    const file = await page.screenshot({encoding:'base64'});
-
-    await browser.close()
-
+  if (event.httpMethod == "OPTIONS") {
 
     return {
       statusCode: 200,
-      body: JSON.stringify(file),
-    }
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error }),
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Methods": "GET, POST, OPTION",
+      },
+    };
+  } 
+  if(event.httpMethod == "POST"){
+    try {
+      const browser = await puppeteer.launch({
+        args: chromium.args,
+        defaultViewport: chromium.defaultViewport,
+        executablePath: false ? await chromium.executablePath('/var/task/node_modules/@sparticuz/chromium/bin') : "C:/Program Files/Google/Chrome/Application/chrome.exe",
+      })
+      try {
+        
+        const page = await browser.newPage()
+        const url = JSON.parse(event.body)['url']
+    
+        await page.goto(url, {
+          timeout:5000
+        });
+    
+        // await page.setViewport({
+        //   width: 1000,
+        //   height: 600,
+        // });
+      
+        const file = await page.screenshot({encoding:'base64'});
+    
+        await browser.close()
+    
+        return {
+          statusCode: 200,
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers": "Content-Type",
+            "Access-Control-Allow-Methods": "GET, POST, OPTION",
+          },
+          body: JSON.stringify(file),
+        }
+      } catch (error) {
+        await browser.close()
+        return {
+          statusCode: 500,
+          body: JSON.stringify({ error }),
+        }
+      }
+    } catch (err) {
+      console.log(err);
+      return {
+        statusCode: 500,
+        body: JSON.stringify({ err }),
+      }
     }
   }
 }
